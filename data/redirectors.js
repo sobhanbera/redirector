0377b2ec70c95f139aadd786923a815825884185
sobhanbera <sobhanbera258@gmail.com>
2022-12-21 15:13:14 +0530

    data for redirector links are added hardcoded here Since github might be having problems related to making API requests to its raw data. The redirector URLs cannot be fetched from the other repos. Solution is here...

---
+++ data/redirectors.js
@@ -0,0 +1,28 @@
+const RedirectorURLs = {
+  resume: "https://sobhanbera.vercel.app",
+  cv: "https://sobhanbera.vercel.app",
+  portfolio: "https://sobhanbera.vercel.app",
+  oldportfolio: "https://sobhanbera1.vercel.app",
+  github: "https://github.com/sobhanbera",
+  google: "https://www.google.com/search?q=sobhan+bera",
+  linkedin: "https://www.linkedin.com/in/sobhanbera",
+  instagram: "https://www.instagram.com/sobhanbera_",
+  dribbble: "https://dribbble.com/sobhanbera",
+  facebook: "https://www.facebook.com/sobhanberaos",
+  codersrank: "https://profile.codersrank.io/user/sobhanbera",
+  codeforces: "https://codeforces.com/profile/sobhanbera",
+  codechef: "https://www.codechef.com/users/sobhanbera",
+  hackerearth: "https://www.hackerearth.com/@sobhanbera",
+  blogs: "https://sobhanblogs.vercel.app",
+  blog: "https://sobhanblogs.vercel.app",
+  opensourceblog:
+    "https://sobhanblogs.vercel.app/deep-dive-to-open-source-with-github?data=1",
+  osblog:
+    "https://sobhanblogs.vercel.app/deep-dive-to-open-source-with-github?data=1",
+  testloop1: "https://sobhanr.vercel.app/testloop2",
+  testloop2: "https://sobhanr.vercel.app/testloop1",
+  // loopa: "http://localhost:3000/loopb",
+  // loopb: "http://localhost:3000/loopa",
+};
+
+export { RedirectorURLs };
